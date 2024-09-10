7433f64f07aa91223cdb34e22046c71e254728b4
liz-lovelace <ivanov0604041@gmail.com>
2024-09-10 19:52:44 +0500

    hotfix

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ app.use(express.static('public'));
 // we have to do this on the backend because the browser throws a CORS error if it tries to access this API
 app.get('/qrng', async (req, res) => {
   try {
-    throw 'abc'
     const response = await fetch('http://lfdr.de/qrng_api/qrng?length=1&format=HEX');
     const data = await response.json();
     res.json(data);
